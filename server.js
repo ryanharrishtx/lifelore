3f2352e8a34e6dfb42d250700602879095349320
ryanharrishtx <139286619+ryanharrishtx@users.noreply.github.com>
2024-01-30 13:07:07 -0600

    working on server and mongo connection

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose'),
+    cors = require('cors'),
+    express = require('express'),
+    keys = require('./keys'),
+    app = express();
+
+const API_PORT = process.env.PORT || 5000;
+const dbURI = `mongodb+srv://${keys.username}:${keys.password}@cluster0.kjzzpxs.mongodb.net/?retryWrites=true&w=majority`
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+const journalSchema = new mongoose.Schema({
+    tags: Array,
+    title: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+const Journal = mongoose.model('Journal', journalSchema);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
